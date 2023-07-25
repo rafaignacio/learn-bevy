44ff7166b51ecd4f9240ddb5db31bcbed3869103
Rafael Ignacio Pinto <ignacio.rafael@gmail.com>
2023-07-25 21:23:59 +0200

    fixing camera

--- src/plugins/camera.rs
+++ src/plugins/camera.rs
@@ -11,7 +11,7 @@ impl Plugin for CameraPlugin {
 fn init_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
     let window = window_query.get_single().unwrap();
     commands.spawn(Camera2dBundle {
-        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 11.0),
         ..default()
     });
 }
